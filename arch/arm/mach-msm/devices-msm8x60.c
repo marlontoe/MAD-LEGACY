7864436c94e11fd0a5d2ce56a25fc60902ab0be0
someone755 <jari.begus@hotmail.com>
2014-06-06 14:39:14 +0200

    cleanup
    Conflicts:
    	arch/arm/mach-msm/devices-msm8x60.c

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -909,9 +909,14 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 300000000,
+			.gpu_freq = 320000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
+		},  
+		{
+			.gpu_freq = 300000000,
+			.bus_freq = 4,
+			.io_fraction = 15,
 		},
 		{
 			.gpu_freq = 266667000,
@@ -919,22 +924,32 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 33,
 		},
 		{
-			.gpu_freq = 177778000,
+			.gpu_freq = 228571000,
 			.bus_freq = 2,
-			.io_fraction = 100,
+			.io_fraction = 50,
 		},
 		{
-			.gpu_freq = 128000000,
+			.gpu_freq = 200000000,
+			.bus_freq = 2,
+			.io_fraction = 65,
+		},
+		{
+			.gpu_freq = 177778000,
+			.bus_freq = 1,
+			.io_fraction = 85,
+		},
+		{
+			.gpu_freq = 145455000,
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
 		{
-			.gpu_freq = 27000000,
+			.gpu_freq = 128000000,
 			.bus_freq = 0,
 		},
 	},
 	.init_level = 0,
-	.num_levels = 5,
+	.num_levels = 8,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
 	.nap_allowed = true,
