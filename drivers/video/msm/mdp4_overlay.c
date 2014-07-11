870b09b763f3510c00dd69b3cffefc33c12804c5
Sultanxda <sultanxda@gmail.com>
2014-07-11 02:49:13 -0700

    msm: mdp4: Enable release fence for LCDC panels

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -4025,7 +4025,7 @@ int mdp4_overlay_commit(struct fb_info *info)
 		mdp4_dsi_video_pipe_commit(0, 1);
 		break;
 	case LCDC_PANEL:
-		mdp4_lcdc_pipe_commit(0, 1);
+		mdp4_lcdc_pipe_commit(0, 1, &release_busy);
 		break;
 #ifdef CONFIG_FB_MSM_HDMI_MSM_PANEL
 	case DTV_PANEL:
